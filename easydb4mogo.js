d96c0f12a89dc76030dddc24d530ef728cfaf439
Abel-Huang <497720975@qq.com>
2017-04-24 19:54:30 +0800

    all file

--- easydb4mogo.js
+++ easydb4mogo.js
@@ -1,3 +1,33 @@
 /**
  * Created by huangjianjin on 2016/12/24.
  */
+var mongoose = require('mongoose');
+
+exports.insertMogo=function () {
+    mongoose.connect('mongodb://localhost/accounts');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        console.log('mongoose opened!');
+        var userSchema = new mongoose.Schema({
+                name:{type: String, unique: true},
+                password:String
+            },
+            {collection: "accounts"}
+        );
+        var User = mongoose.model('accounts', userSchema);
+
+        User.findOne({name:"WangEr"}, function(err, doc){
+            if(err)
+                console.log(err);
+            else
+                console.log(doc.name + ", password - " + doc.password);
+        });
+
+        var lisi = new User({name:"WangEr", password:"654321"});
+        lisi.save(function(err, doc){
+            if(err)console.log(err);
+            else console.log(doc.name + ' saved');
+        });
+    });
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,29 @@
 /**
  * Created by huangjianjin on 2016/12/20.
  */
+// this is a function use for certain if a obj in an array
+exports.contains=function (arr, obj) {
+    var i = arr.length;
+    while (i--) {
+        if (arr[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+//delete a certain obj from array.
+Array.prototype.indexOf = function(val) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == val) return i;
+    }
+    return -1;
+};
+Array.prototype.remove = function(val) {
+    var index = this.indexOf(val);
+    if (index > -1) {
+        this.splice(index, 1);
+    }
+};
+exports.removeFromArray=function (arr,obj) {
+    arr.remove(obj);
+}
