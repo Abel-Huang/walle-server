522a68570c4cd3e781b7810de25464383918dbc1
dingyinghao <dingyinghao@zhijiatech.com>
2017-04-30 09:52:07 +0800

    调整部分代码

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
 var net = require('net');
-var easymogo=require('./easymogo');
-var utils=require('./utils');
+var easymogo = require('./easymogo');
+var utils = require('./utils');
 
 // easymogo.queryMogo({"col1":"213"});
 
-var connectMap=new Array();
-var clientObj=new Object();
-clientObj.setIP=function (clientIP) {
-    this.clientIP=clientIP;
+var connectMap = new Array();
+var clientObj = new Object();
+clientObj.setIP = function (clientIP) {
+	this.clientIP = clientIP;
 }
-clientObj.setPort=function (clientPort) {
-    this.clientPort=clientPort;
+clientObj.setPort = function (clientPort) {
+	this.clientPort = clientPort;
 }
 
-var listenPort = 8989;//监听端口
-var server = net.createServer(function(socket){
-    // 我们获得一个连接 - 该连接自动关联一个socket对象
-    console.log('connect: ' +socket.remoteAddress + ':' + socket.remotePort+'  client has connected');
-    clientObj.setIP(socket.remoteAddress);
-    clientObj.setPort(socket.remotePort);
-    socket.write('hello');
-    //接收到数据,并对收到的消息进行对应的处理
-    socket.on('data',function(data){
-		data=String(data);
-        console.log('recv:' + data);
-        easymogo.insertMogo(data);
-		if(data.replace('\n','')==='hello')
-		{
-			connectMap.push(clientObj);
+var listenPort = 8989; //监听端口
+var server = net.createServer(function (socket) {
+		// 我们获得一个连接 - 该连接自动关联一个socket对象
+		console.log('connect: ' + socket.remoteAddress + ':' + socket.remotePort + '  client has connected');
+		clientObj.setIP(socket.remoteAddress);
+		clientObj.setPort(socket.remotePort);
+		socket.write('hello');
+		//接收到数据,并对收到的消息进行对应的处理
+		socket.on('data', function (data) {
+			data = String(data);
+			console.log('recv:' + data);
+			var obj = new Object();
+			obj.bluetooth=new Array();
+			obj.wifi=new Array();
+			obj.pos_x=0;
+			obj.pos_y=0;
+			easymogo.insertMogo(obj);
+			
+			// if (data.replace('\n', '') === 'hello') {
+				// connectMap.push(clientObj);
+				// console.log(connectMap.length);
+			// }
+			
+			// var resultStr=String(data);
+			// //case0:
+			// if(resultStr.replace('\n','')==='hello')
+			//     console.log('小车');
+			// //case1：wifi
+			// var str=resultStr.replace(/\+CWLAP:\(/g,'');
+			// str=str.replace(/"/g,'');
+			// var strArray=str.split(")");
+			// var easymogo=require('./easymogo');
+			//easymogo.insertMogo(strArray);
+			//case2:bluetooth
+		});
+
+		//数据错误事件
+		socket.on('error', function (exception) {
+			console.log('socket error:' + exception);
+			socket.end();
+		});
+		//客户端关闭事件
+		socket.on('close', function (data) {
+			console.log('close: ' + socket.remoteAddress + ' ' + socket.remotePort + '  client has disconnected');
+			clientObj.setIP(socket.remoteAddress);
+			clientObj.setPort(socket.remotePort);
+			if (utils.contains(connectMap, clientObj)) {
+				utils.removeFromArray(connectMap, clientObj);
+			}
 			console.log(connectMap.length);
-		}
-        // var resultStr=String(data);
-        // //case0:
-        // if(resultStr.replace('\n','')==='hello')
-        //     console.log('小车');
-        // //case1：wifi
-        // var str=resultStr.replace(/\+CWLAP:\(/g,'');
-        // str=str.replace(/"/g,'');
-        // var strArray=str.split(")");
-        // var easymogo=require('./easymogo');
-        //easymogo.insertMogo(strArray);
-        //case2:bluetooth
-    });
-
-    //数据错误事件
-    socket.on('error',function(exception){
-        console.log('socket error:' + exception);
-        socket.end();
-    });
-    //客户端关闭事件
-    socket.on('close',function(data){
-        console.log('close: ' + socket.remoteAddress + ' ' + socket.remotePort+'  client has disconnected');
-        clientObj.setIP(socket.remoteAddress);
-        clientObj.setPort(socket.remotePort);
-        if(utils.contains(connectMap,clientObj)){
-            utils.removeFromArray(connectMap,clientObj);
-        }
-        console.log(connectMap.length);
-    });
-}).listen(listenPort);
+		});
+	}).listen(listenPort);
 
 //服务器监听事件
-server.on('listening',function(){
-    console.log("server listening:" + server.address().port);
+server.on('listening', function () {
+	console.log("server listening:" + server.address().port);
 });
 
 //服务器错误事件
-server.on("error",function(exception){
-    console.log("server error:" + exception);
+server.on("error", function (exception) {
+	console.log("server error:" + exception);
 });
-
--- easymogo.js
+++ easymogo.js
@@ -4,67 +4,46 @@
 
 // this module use for mongodb
 var MongoClient = require('mongodb').MongoClient;
-var mongo=new MongoClient();
-var strArray;
+
+//db config
 var DB_CONN_STR = 'mongodb://admin:admin@localhost:27017/admin';
-exports.insertMogo=function (strArray) {
-    mongo.connect(DB_CONN_STR,function (err,db) {
-        if(err){
-            console.log('Error:'+ err);
-            return;
-        }
-        var myDB=db.db('walle');
-        myDB.collection("wifi",function (err,collection) {
-            // for(var i=0;i<strArray.length;i++) {
-                // var elem=strArray[i].split(',');
-                // var data = [{"ecn":elem[0].replace('\r','').replace('\n',''),"ssid":elem[1],"rssi":elem[2],"mac":elem[3],"ch":elem[4]}];
-                // collection.insert(data,function (err,result) {
-                    // if(err)
-                    // {
-                        // console.log('Error:'+ err);
-                        // return;
-                    // }
-                    // else
-                        // console.log('Result:'+ result);
-                // });
-            // }
-			var data={"col1":strArray};
-			collection.insert(data,function (err,result) {
-                if(err)
-                {
-                    console.log('Error:'+ err);
-                    return;
-                }
-                else
-                    console.log('Result:'+ result);
-                }
-			);
-            myDB.close(err,db);
-        });
-    });
+var DB_NAME = 'walle';
+var COLLECTION_NAME = 'location';
+
+exports.insertMogo = function (insert_doc) {
+	MongoClient.connect(DB_CONN_STR, function (err, db) {
+		if (err) {
+			console.log('Error:' + err);
+			return;
+		}
+		db = db.db(DB_NAME);
+		db.collection(COLLECTION_NAME, function (err, collection) {
+			if (typeof(insert_doc) != "object") {
+				console.log("insertMogo params is not a object");
+				return;
+			}
+			collection.insert(insert_doc, function (err, result) {
+				if (err) {
+					console.log('Error:' + err);
+					return;
+				} else
+					console.log('Result:' + result);
+			});
+			db.close(err, db);
+		});
+	});
 }
 
-var queryStr;
-exports.queryMogo=function(queryStr){
-    var selectData = function(db, callback) {
-        //连接到表
-        var collection = db.collection('wifi');
-        //查询数据
-        collection.find(queryStr).toArray(function(err, result) {
-            if(err)
-            {
-                console.log('Error:'+ err);
-                return;
-            }
-            callback(result);
-        });
-    }
-
-    MongoClient.connect(DB_CONN_STR, function(err, db) {
-		db=db.db('walle');
-        selectData(db, function(result) {
-            console.log(result);
-            db.close();
-        });
-    });
+exports.queryMogo = function (queryStr) {
+	MongoClient.connect(DB_CONN_STR, function (err, db) {
+		db = db.db(DB_NAME);
+		db.collection(COLLECTION_NAME).find(queryStr).toArray(function (err, result) {
+			if (err) {
+				console.log('Error:' + err);
+				return;
+			}
+			console.log(result);
+			db.close();
+		});
+	});
 }
